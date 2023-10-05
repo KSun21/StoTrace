176ae17748b8c89ecbda81997b6ad6b78e9b1520
Gustavo <gustavo.aroeira@gmail.com>
2023-10-04 22:33:19 -0400

    added complex vectors to hutchinson

--- src/Hutchinson.jl
+++ src/Hutchinson.jl
@@ -1,4 +1,5 @@
 using StoTrace
+using Distributions
 
 export hutchinson
 
@@ -6,13 +7,18 @@ function rademacher(n)
     return rand([-1.0, 1.0], n)
 end
 
-function hutchinson(m, M, cb)
+function complex_unit(n)
+    dist = Uniform(0, 2π)
+    return [exp(-im*θ) for θ = rand(dist, n)]
+end
+
+function hutchinson(m, M, cb; real=true)
 
     out = 0.0
 
     l = size(M, 1)
     for i = 1:m
-        v = rademacher(l)
+        v = real ? rademacher(l) : complex_unit(l)
         out += inner(M, v, cb)
     end
 
