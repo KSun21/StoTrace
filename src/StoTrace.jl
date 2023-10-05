b58ac5601aff467bb4229137a633a4e0565ba60b
Gustavo <gustavo.aroeira@gmail.com>
2023-10-04 22:33:50 -0400

    adapted trace_estimate with the new features

--- src/StoTrace.jl
+++ src/StoTrace.jl
@@ -6,9 +6,9 @@ using LinearAlgebra
 
 include("Chebyshev.jl")
 include("Hutchinson.jl")
+include("DOS.jl")
 
-
-function trace_estimate(f::Function, A, λmin, λmax; cheb_order=4, cheb_npoints=1000, k=10, ϵ=0.01, verb=false)
+function trace_estimate(f::Function, A, λmin, λmax; cheb_order=4, cheb_npoints=1000, k=10, ϵ=0.01, verb=false, real=true, jackson=false)
 
     pln(x) = verb ? println(x) : nothing
 
@@ -16,14 +16,14 @@ function trace_estimate(f::Function, A, λmin, λmax; cheb_order=4, cheb_npoints
     a = (λmax - λmin)/(2-ϵ)
 
     pln("Getting Chebyshev fit using order = $cheb_order")
-    cb = chebyshev_fit(x->f(a*x+b), cheb_npoints, order=cheb_order)
+    cb = chebyshev_fit(x->f(a*x+b), cheb_npoints, order=cheb_order, jackson=jackson)
 
     pln("Done.")
     pln("Last Chebyshev coefficient: $(cb.coef[end])")
 
     pln("Starting Hutchinson iterations")
     sA = (A - b*I)/a
-    out = hutchinson(k, sA, cb)
+    out = hutchinson(k, sA, cb, real=real)
 
     pln("Final estimated trace: $out")
 
